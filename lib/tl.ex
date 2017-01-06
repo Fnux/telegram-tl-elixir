b4dee9ac4b5351ed42306fa8ebc65423f38dee95
Timothée Floure <timothee.floure@fnux.ch>
2017-01-06 15:32:08 +0100

    Initial commit. (mix new)

---
+++ lib/tl.ex
@@ -0,0 +1,18 @@
+defmodule TL do
+  @moduledoc """
+  Documentation for TL.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TL.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tl_test.exs
@@ -0,0 +1,8 @@
+defmodule TLTest do
+  use ExUnit.Case
+  doctest TL
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
