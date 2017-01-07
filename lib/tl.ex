4f7afc2efe7722465733825a71b087ae119e7b5e
Timothée Floure <timothee.floure@fnux.ch>
2017-01-07 20:33:26 +0100

    Basic moduledoc for TL

--- lib/tl.ex
+++ lib/tl.ex
@@ -1,6 +1,9 @@
 defmodule TL do
   @moduledoc """
-  Documentation for TL. @TODO
+  This library allows you to serialize and deserialize elements of the
+  [TL Language](https://core.telegram.org/mtproto/TL). It was originally
+  designed to be used by
+  [telegram-mt-elixir](https://github.com/Fnux/telegram-mt-elixir).
   """
 
   @doc """
