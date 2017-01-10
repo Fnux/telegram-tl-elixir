60a6863c0fae2d2d635c151f1ef9f4b64056bb6f
Timothée Floure <timothee.floure@fnux.ch>
2017-01-10 21:28:55 +0100

    Update package to 0.0.4-alpha

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TL.Mixfile do
 
   def project do
     [app: :telegram_tl,
-     version: "0.0.3-alpha",
+     version: "0.0.4-alpha",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
