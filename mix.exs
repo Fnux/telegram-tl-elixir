5b9142eaa4c9b16ff6667ac455eef7c515a23710
Timothée Floure <timothee.floure@fnux.ch>
2017-04-26 19:03:31 +0200

    Bump to version 0.0.7-alpha

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TL.Mixfile do
 
   def project do
     [app: :telegram_tl,
-     version: "0.0.6-alpha",
+     version: "0.0.7-alpha",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
